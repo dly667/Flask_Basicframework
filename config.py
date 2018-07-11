a3b836edb36e6abdb938713d000086060b35d8dd
mac <dly667@macs-MacBook-Pro.local>
2018-07-11 21:30:34 +0800

    first

---
+++ config.py
@@ -0,0 +1,31 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY") or 'hard to guess string'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    FLASK_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASK_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
+    FLASK_ADMIN = os.environ.get("FLASK_ADMIN")
+
+    @staticmethod
+    def init_app(app):
+        pass
+class DevelopmentsConfig(Config):
+    DEBUG = True
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///'+os.path.join(basedir, 'data-dev.sqlite')
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///'+os.path.join(basedir, 'data.sqlite')
+config = {
+    'development':DevelopmentsConfig,
+    'production':ProductionConfig,
+    'default':DevelopmentsConfig
+}
